210b8ad100fd78addcdeb22106c941fa935c9fd6
ArtScape Developer <developer@artscape.studio>
2026-01-06 22:14:23 +0530

    Fix import casing for AboutUs component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import SingleProduct from "./structure/products/SingleProduct";
 import Pagenotfound from './components/Pagenotfound';
 import ScrollToTop from "./structure/ScrollToTop";
 import Accessories from "./components/Accessories";
-import AboutUs from "./components/aboutus";
+import AboutUs from "./components/Aboutus";
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
@@ -26,22 +26,22 @@ const App = () => {
   return (
     <>
       <Header theme={theme} toggleTheme={toggleTheme} />
-      <ScrollToTop/>
+      <ScrollToTop />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="products" element={<Products />} />
-          <Route path="/products/:slug" element={<SingleProduct/>} />
-          <Route path="aboutUs" element={<AboutUs/>} />
-          <Route path="gallery" element={<Pagenotfound/>} />
-          <Route path="contactUs"  element={<Pagenotfound/>} />
-          <Route path="accessories" element={<Accessories/>} />
-          <Route path="product" element={<Pagenotfound/>}/>
-          <Route path="help" element={<Pagenotfound/>} />
-          <Route path="*" element={<Pagenotfound/>} />
+          <Route path="/products/:slug" element={<SingleProduct />} />
+          <Route path="aboutUs" element={<AboutUs />} />
+          <Route path="gallery" element={<Pagenotfound />} />
+          <Route path="contactUs" element={<Pagenotfound />} />
+          <Route path="accessories" element={<Accessories />} />
+          <Route path="product" element={<Pagenotfound />} />
+          <Route path="help" element={<Pagenotfound />} />
+          <Route path="*" element={<Pagenotfound />} />
         </Routes>
       </main>
-      <Footer/>
+      <Footer />
     </>
   )
 }
